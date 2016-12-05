dd6aaffbbe2d01374c5631f95b7a106b3265ea33
Anton <antonkarabeinikov@gmail.com>
2016-12-05 11:20:50 +0200

    refactor3
    according task:
    -Should be performed search on 1-3 pages by Top
    -Top Name and Price should be sent to DB
    insted:
    -Selecting by Top on 1 page
    -Top Name and Price are sent to DB

--- src/test/java/ua/rozetka/tests/HelpPage.java
+++ src/test/java/ua/rozetka/tests/HelpPage.java
@@ -22,19 +22,22 @@ public class HelpPage {
     By phoneTVandElectr = By.linkText("Телефоны, ТВ и электроника");
     By phones = By.linkText("Телефоны");
     By smartphones = By.linkText("Смартфоны");
+    By dropDownLink = By.name("drop_link");
+    By dropDownItemTop = By.linkText("популярные");
+    By titledPhoneNames = By.cssSelector(".g-i-tile-i-title.clearfix>a");
+    By titledPhonePrices = By.cssSelector(".g-price-uah");
+    private String query = "INSERT INTO smartphones (name, price)" + " values (?, ? )";
 
 
     public HelpPage(WebDriver driver) {
         this.driver = driver;
     }
 
-
     public void loginToRozetka(String userid, String pass) {
         driver.findElement(enter).click();
         driver.findElement(username).sendKeys(userid);
         driver.findElement(password).sendKeys(pass);
         driver.findElement(loginButton).click();
-
     }
 
     public void navigateToSmartphones() {
@@ -44,14 +47,14 @@ public class HelpPage {
     }
 
     public void selectFromDropDown() {
-        driver.findElement(By.name("drop_link")).click();
-        driver.findElement(By.linkText("популярные")).click();
+        driver.findElement(dropDownLink).click();
+        driver.findElement(dropDownItemTop).click();
     }
 
     public void phoneNameAdnPriceSendToDB(int index) {
-        WebElement namePhone1 = driver.findElements(By.cssSelector(".g-i-tile-i-title.clearfix>a")).get(index);
+        WebElement namePhone1 = driver.findElements(titledPhoneNames).get(index);
         String nameToPrint1 = namePhone1.getText();
-        WebElement pricePhone1 = driver.findElements(By.cssSelector(".g-price-uah")).get(index);
+        WebElement pricePhone1 = driver.findElements(titledPhonePrices).get(index);
         String pricePhoneToPrint1 = pricePhone1.getText();
 //        System.out.println(nameToPrint1);
 //        System.out.println(pricePhoneToPrint1);
@@ -62,8 +65,6 @@ public class HelpPage {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost/rozetka?user=root&password=");
-            String query = "INSERT INTO smartphones (name, price)"
-                    + " values (?, ? )";
             // create the mysql insert preparedstatement
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             //preparedStmt1.setInt(1, 01);
@@ -77,8 +78,6 @@ public class HelpPage {
             System.err.println(e.getMessage());
         }
     }
-
-
 }
 
 
--- src/test/java/ua/rozetka/tests/RozetkaTest.java
+++ src/test/java/ua/rozetka/tests/RozetkaTest.java
@@ -14,16 +14,13 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 
 
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class RozetkaTest {
-
     private WebDriver driver;
     private String baseUrl;
-
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
@@ -35,20 +32,25 @@ public class RozetkaTest {
 
     @Test
     public void testSendNameAndPriceToDB() throws Exception {
-        HelpPage helpPage =new HelpPage(driver);
+        HelpPage helpPage = new HelpPage(driver);
         helpPage.loginToRozetka("fejoz@rootfest.net", "Test1234");
         helpPage.navigateToSmartphones();
+        //according task:
+        //-Should be performed search on 1-3 pages by Top
+        //-Top Name and Price should be sent to DB
+        //instead:
+        //-Selecting by Top on 1 page
+        //-First 4 Top Name and Price was sent to DB
         helpPage.selectFromDropDown();
         helpPage.phoneNameAdnPriceSendToDB(0);
         helpPage.phoneNameAdnPriceSendToDB(1);
         helpPage.phoneNameAdnPriceSendToDB(2);
         helpPage.phoneNameAdnPriceSendToDB(3);
     }
+
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.quit();
-
-
-        }
     }
+}
 
